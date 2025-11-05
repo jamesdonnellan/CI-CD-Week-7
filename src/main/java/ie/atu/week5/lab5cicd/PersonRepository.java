05beea3920493e4a9ffd5afac1b4076cd581cf7c
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-05 15:46:33 +0000

    Current Progress on finalising DELETE crud operation

--- src/main/java/ie/atu/week5/lab5cicd/PersonRepository.java
+++ src/main/java/ie/atu/week5/lab5cicd/PersonRepository.java
@@ -2,10 +2,13 @@ package ie.atu.week5.lab5cicd;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long>
 {
     Optional <Person> findByEmployeeId(String employeeId);
+
+    List<Person> id(Long id);
 }
 
