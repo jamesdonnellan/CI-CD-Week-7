66fed242948233af81accb17a407d96c505765ca
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-05 14:16:04 +0000

    Created Person Entity Class to tell springboot it that it represents a table in the database

---
+++ src/main/java/ie/atu/week5/lab5cicd/Person.java
@@ -0,0 +1,19 @@
+package ie.atu.week5.lab5cicd;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name= "persons")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Person
+{
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String email;
+  private String employeeId;
+  private String position;
+  private String department;
+}
