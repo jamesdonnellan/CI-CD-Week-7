0e1ba539b9bb5ff143f26fe66f910308a0736487
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-05 14:04:55 +0000

    Created New Project - CI/CD Lab 7 - SpringBoot and h2

---
+++ src/main/java/ie/atu/week5/lab5cicd/Lab5cicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5.lab5cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5cicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5cicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5/lab5cicd/Lab5cicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5.lab5cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5cicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
