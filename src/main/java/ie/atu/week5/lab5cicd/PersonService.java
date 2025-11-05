3b08229af0b049ddbda347dcbf34311b3f747228
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2025-11-05 14:27:10 +0000

    Created Person Service layer to call repository methods

---
+++ src/main/java/ie/atu/week5/lab5cicd/PersonService.java
@@ -0,0 +1,31 @@
+package ie.atu.week5.lab5cicd;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService
+{
+    private final PersonRepository repo;
+    public PersonService(PersonRepository repo)
+    {
+        this.repo = repo;
+    }
+
+    public Person create (Person p)
+    {
+        return repo.save(p);
+    }
+    public List<Person> findAll()
+    {
+        return repo.findAll();
+    }
+
+    public Person findByEmployeeId(String id)
+    {
+        return repo.findByEmployeeId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Person not found"));
+    }
+}
